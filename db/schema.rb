89c8164e360147592cd1989a992acbb248a2d7d0
Saint-Jean Germila <germila@noukod.com>
2019-01-21 19:03:32 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20190121161608) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
+
 end
